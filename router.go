345dd4c43e2d6c320ff2d9e0886b37e5ed8814bd
MOONMOON8637 <53054219+MOONMOON8637@users.noreply.github.com>
2022-09-14 19:14:52 +0800

    Add files via upload

---
+++ router.go
@@ -0,0 +1,12 @@
+package routers
+
+import (
+	"github.com/99MyCql/chatRoom/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/chatRoom", &controllers.ServerController{})
+	beego.Router("/chatRoom/WS", &controllers.ServerController{}, "get:WS")
+}
